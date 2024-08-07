84c013a95c4491589a7fb3886b82478b249fed5a
Roheemoh Abdulazeez <144372737+reehmahcodes@users.noreply.github.com>
2024-08-07 15:42:11 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.getElementById('nav');
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 50) {
+        nav.classList.add('scrolled');
+      } else {
+        nav.classList.remove('scrolled');
+      }
+    });
+  }); 
+     
